e1879ce1ff62b821bcf5ac40413cd48657e05f3f
Dimitar Lilov <dimitar.lilov.mici@gmail.com>
2019-04-01 12:20:49 +0300

    Added dropdown categories menu

--- src/Web/TicketStore.Web/ClientApp/src/app/components/shared/header/header.component.ts
+++ src/Web/TicketStore.Web/ClientApp/src/app/components/shared/header/header.component.ts
@@ -1,25 +1,32 @@
 import { Component ,OnInit } from '@angular/core';
-import { AuthService, IdentityService, CartService } from '../../../services';
+import { AuthService, IdentityService, CartService, CategoryService } from '../../../services';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Category } from '../../../domain';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public categories : Category[];
+  public isAdmin : boolean = false;
+  public isUserAuthorized: boolean = false;
+  public userEmail: string = null;
+  public firstName: string = null;
+
   faShoppingCart = faShoppingCart;
   cartItems = 0;
+  isExpanded = false;
+  dropdownLi : string = "nav-item dropdown";
+  dropdownMenu : string = "dropdown-menu";
+
   constructor(private authService: AuthService,
     private identityService: IdentityService,
-    private cartService: CartService) {
+    private cartService: CartService,
+    private categoryService: CategoryService) {
 }
-
-public isAdmin : boolean = false;
-public isUserAuthorized: boolean = false;
-public userEmail: string = null;
-public firstName: string = null;
-isExpanded = false;
 
 public logout(): void {
     this.authService.logout();
@@ -33,8 +40,21 @@ toggle() {
   this.isExpanded = !this.isExpanded;
 }
 
+expand() {
+  this.dropdownLi.endsWith('show') 
+  ? this.dropdownLi = "nav-item dropdown" 
+  : this.dropdownLi = "nav-item dropdown show";
+
+  this.dropdownMenu.endsWith('show')
+  ? this.dropdownMenu = "dropdown-menu"
+  : this.dropdownMenu = "dropdown-menu show";
+}
 
 ngOnInit(): void {
+  this.categoryService.getAll().subscribe((categories) => {
+    this.categories = categories;
+  });
+
   this.isAdmin = this.authService.isAdmin();
   this.authService.isAuthorized$.subscribe(
     (isAuthorized: boolean) => {
