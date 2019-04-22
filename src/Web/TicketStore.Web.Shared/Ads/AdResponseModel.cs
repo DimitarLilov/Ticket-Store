f974ccaa0448a2c2fdc0b6a1bb9698e8637f317a
Dimitar Lilov <dimitar.lilov.mici@gmail.com>
2019-04-22 15:11:45 +0300

    Fixed AdResponseModel

--- src/Web/TicketStore.Web.Shared/Ads/AdResponseModel.cs
+++ src/Web/TicketStore.Web.Shared/Ads/AdResponseModel.cs
@@ -1,20 +1,28 @@
 ﻿namespace TicketStore.Web.Shared.Ads
 {
+    using AutoMapper;
     using System;
     using TicketStore.Common.Mapping;
     using TicketStore.Data.Models;
     using TicketStore.Web.Shared.Events;
 
-    public class AdResponseModel : IMapFrom<Ad>
+    public class AdResponseModel : IMapFrom<Ad>, IHaveCustomMappings
     {
         public EventResponseModel Event { get; set; }
 
-        public AdTypeResponseModel Type { get; set; }
+        public string Type { get; set; }
 
         public bool Active { get; set; }
 
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<Ad, AdResponseModel>()
+                .ForMember(x => x.Type,
+                    m => m.MapFrom(c => c.Type.Type));
+        }
     }
 }
